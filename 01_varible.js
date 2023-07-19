c1ffac31f4b56b69d6dd70bdfb8e7b10f2ca7d04
NitishRajuC <nitishchougule10@gmail.com>
2023-07-20 00:41:32 +0530

    Basic Program
    Basic Program

---
+++ 01_varible.js
@@ -0,0 +1,14 @@
+var myName;//varible declaration
+myName = "Nitish";//varible initialization
+console.log(myName);
+
+var city = "pune";
+
+console.log(city);  // declaration and initilization
+
+
+var pin;
+console.log(pin);//v d
+
+var myFirstName = "Nitish";
+console.log(myFirstName)
