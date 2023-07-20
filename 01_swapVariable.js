2e13c3a0e11722ffcf60d0933f6edb424abf4fbc
NitishRajuC <nitishchougule10@gmail.com>
2023-07-20 10:47:02 +0530

    Assignment on variable
    Assignment on variable

---
+++ 01_swapVariable.js
@@ -0,0 +1,22 @@
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log("Before swaping==>", 'sweety-',sweety,'cutie-',cutie);
+
+var temp = sweety;
+sweety = cutie;
+cutie  = temp;
+console.log("After swaping==>",'sweety-',sweety,'cuite-',cutie);
+console.log("=============================================================");
+
+
+console.log("Swap three variable value");
+
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("value Befor swap=" ,'num1-',num1,'num2-',num2,'nun3-',num3);
+
+num1 = num2;
+num2 = num3;
+num3 = num1;
+console.log("value Aefor swap=" ,'num1-',num1,'num2-',num2,'nun3-',num3);
